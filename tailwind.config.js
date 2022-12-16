32f598bc2bc394e88a321095c749d0c9e2e737a1
taironcoelho <taironcoelho@gmail.com>
2022-12-16 18:47:10 +0100

    chore: clean tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,14 +17,6 @@ module.exports = {
         300: '#FFCDAA',
         200: '#FFF0E6',
       },
-      blue: {
-        700: '#0169C9',
-        600: '#0078E6',
-        500: '#2898FF',
-        400: '#49A8FF',
-        300: '#92CBFF',
-        200: '#DBEEFF',
-      },
       deepPurple: {
         600: '#1C0E32',
         500: '#1F0F38',
@@ -52,27 +44,6 @@ module.exports = {
         250: '#F2F2F2',
         200: '#F5F5F5',
         100: '#FAFAFA',
-      },
-      green: {
-        600: '#5FC02A',
-        500: '#78D644',
-        400: '#93DE6A',
-        300: '#BEEBA5',
-        200: '#F1FBEC',
-      },
-      red: {
-        600: '#EA1300',
-        500: '#FF4433',
-        400: '#FF5849',
-        300: '#FF9B92',
-        200: '#FFECEB',
-      },
-      yellow: {
-        600: '#EDB900',
-        500: '#FFC700',
-        400: '#FFD749',
-        300: '#FFE792',
-        200: '#FFFBEB',
       },
     },
     fontFamily: { // https://tailwindcss.com/docs/font-family#font-families
@@ -113,86 +84,6 @@ module.exports = {
       banner: ['15px', '24px'],
       'translation-label': ['11px', '12px'],
       'rich-text-area': ['14px', '32px'],
-    },
-    boxShadow: { // https://tailwindcss.com/docs/box-shadow#box-shadows
-      1: '0px 1px 4px rgba(26, 26, 26, 0.05)',
-      2: '0px 2px 8px rgba(26, 26, 26, 0.05)',
-      3: '0px 4px 24px rgba(0, 0, 0, 0.05)',
-      'side-panel': '4px 0px 16px rgba(26, 26, 26, 0.05)',
-      'hue-slider-knob': '0px 1px 2px rgba(0, 0, 0, 0.15)',
-      left: '-4px 0px 16px rgba(26, 26, 26, 0.05), -1px 0px 4px rgba(0, 0, 0, 0.05)',
-      top: '0px -4px 24px rgba(0, 0, 0, 0.05)',
-      none: 'none',
-    },
-    minHeight: theme => ({
-      12: theme('height.12'),
-    }),
-    extend: {
-      minWidth: theme => ({
-        4: theme('width.4'),
-        5: theme('width.5'),
-        64: theme('width.64'),
-      }),
-      maxWidth: theme => ({
-        40: theme('width.40'),
-        56: theme('width.56'),
-        64: theme('width.64'),
-        'table-column': '24ch',
-      }),
-      margin: {
-        '-9/16': '-0.5625rem',
-      },
-      padding: {
-        3.25: '0.8125rem',
-      },
-      borderWidth: {
-        9: '9px',
-      },
-      spacing: {
-        88: '22rem',
-        112: '28rem',
-        135: '33.75rem',
-      },
-      width: {
-        '1/10': '10%',
-        '3/10': '30%',
-        '7/10': '70%',
-        '9/10': '90%',
-        82: '20.5rem',
-        100: '25rem',
-      },
-      height: {
-        43: '10.75rem',
-      },
-      keyframes: {
-        'slide-right-in': {
-          '0%': { transform: 'translate(100vw, 0)' },
-          '100%': { transform: 'translate(0vw, 0)' },
-        },
-        'slide-right-out': {
-          '0%': { transform: 'translate(0vw, 0)' },
-          '100%': { transform: 'translate(100vw, 0)' },
-        },
-        'slide-left-in': {
-          '0%': { transform: 'translate(-100vw, 0)' },
-          '100%': { transform: 'translate(0vw, 0)' },
-        },
-        'slide-left-out': {
-          '0%': { transform: 'translate(0vw, 0)' },
-          '100%': { transform: 'translate(-100vw, 0)' },
-        },
-      },
-      animation: {
-        'slide-right-in': 'slide-right-in 500ms linear',
-        'slide-right-out': 'slide-right-out 500ms linear',
-        'slide-left-in': 'slide-left-in 500ms linear',
-        'slide-left-out': 'slide-left-out 500ms linear',
-      },
-      zIndex: {
-        'side-panel': '60',
-        'modal-dialog': '70',
-        toast: '80',
-      },
     },
   },
   plugins: [],
